3a41ae84b91ea39b9600a8567f504e870062b814
Juan Pablo Pino Bravo <jp.pinob@gmail.com>
2025-03-07 14:17:27 +0100

    Delete build__unspec__

--- build___unspec__/test/socket/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/blunux-repos/azmq/test/socket
-# Build directory: /workspaces/blunux-repos/azmq/build___unspec__/test/socket
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/workspaces/blunux-repos/azmq/build___unspec__/test/socket/test_socket_include-b12d07c.cmake")
